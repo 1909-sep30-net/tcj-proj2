b319e081b886810846fd88d484b24127b3206d0b
James <oneinfinity@yahoo.com>
2019-11-01 13:36:18 -0500

    data annotations for user

--- HelpByPros.BusinessLogic/User.cs
+++ HelpByPros.BusinessLogic/User.cs
@@ -5,21 +5,34 @@ namespace HelpByPros.BusinessLogic
     public class User : IUser
     {
         /// <summary>
-        /// firstname
+        /// Id for User Table DB
         /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// First Name
+        /// </summary>
+        [Required(ErrorMessage = "First Name is Required.")]
         [Display(Name = "First Name")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The Input Must be Letters"), Required(AllowEmptyStrings = false)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The Input Must be Letters")]
         public string FirstName { get; set; }
+
         /// <summary>
-        /// lastname 
+        /// Last Name 
         /// </summary>
+        [Required(ErrorMessage = "Last Name is Required.")]
         [Display(Name = "Last Name")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The Input Must be Letters"), Required(AllowEmptyStrings = false)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "The Input Must be Letters")]
         public string LastName { get; set; }
+
         /// <summary>
-        /// phone should be a regex
+        /// Phone Number
         /// </summary>
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Provided Phone Number not Valid")]
         public int Phone { get; set; }
+
         /// <summary>
         /// To notify a question has been raised or answer has been answered
         /// </summary>
@@ -27,27 +40,33 @@ namespace HelpByPros.BusinessLogic
         [Display(Name = "Email Address")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Must Enter Correct Email Address.")]
         public string Email { get; set; }
+
         /// <summary>
-        /// user name
+        /// username
         /// </summary>
         [Required]
         [Display(Name = "Username: 3-12 Characters")]
         [StringLength(12, MinimumLength = 3, ErrorMessage = "Must be Between 3 and 12 Characters.")]
         public string Username { get; set; }
+
         /// <summary>
         /// password 
         /// </summary>
-        [Required]
-        [Display(Name = "Username: 4-12 Characters")]
+        [Required(ErrorMessage = "Password is required")]
         [StringLength(12, MinimumLength = 4, ErrorMessage = "Must be Between 4 and 12 Characters.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(12, MinimumLength = 4, ErrorMessage = "Must be Between 4 and 12 Characters.")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
         /// <summary>
         /// adding pictures in the future
         /// </summary>
+        
         public byte[] Profile_Pic { get; set; }
-
-        public int Id { get; set; }
-
     }
 }
