2e143177b8ea2033bf141d578ae55ad8b4853f9f
Tony Liang <powerstargreen@gmail.com>
2019-11-01 14:01:41 -0500

    Started on Repo class Worked on Iuserrepo chalie worked on Iforumrepo implement Forum repo class

---
+++ HelpByPros.BusinessLogic/IRepo/IUserRepo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HelpByPros.BusinessLogic.IRepo
+{
+    public interface IUserRepo
+    {
+
+        #region Adding information to db
+        /// <summary>
+        /// Add a Member to the DB
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        Task AddMemberAsync(Member m);
+
+        /// <summary>
+        /// Add a Professional to the DB
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        Task AddProfessionalAsync(Professional p);
+
+        /// <summary>
+        /// Add an Admin to the db
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        Task AddAdminAsync(Admin a);
+
+        #endregion
+
+
+        #region Getting a List of Info
+        /// <summary>
+        /// getting a list of Members
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+
+        Task<IEnumerable <Member>> GetMemberListAsync();
+        /// <summary>
+        /// getting a list of professional 
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable <Professional>> GetProfessionalListAsync();
+        /// <summary>
+        /// getting a list admin
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Admin>> GetAdminListAsync();
+
+        /// <summary>
+        /// getting a single member 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Member> GetAMemberAsync(int id);
+        /// <summary>
+        /// getting a professional
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Member> GetAProfessionalAsync(int id);
+
+        /// <summary>
+        /// getting a admin
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Member> GetAAdminAsync(int id);
+        #endregion
+
+
+
+
+    }
+}
---
+++ HelpByPros.DataAccess/Repo/UserRepo.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelpByPros.BusinessLogic;
+using HelpByPros.BusinessLogic.IRepo;
+
+namespace HelpByPros.DataAccess.Repo
+{
+    public class UserRepo : IUserRepo
+    {
+        #region Add information to db
+        public Task AddAdminAsync(Admin a)
+        {
+
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddMemberAsync(Member m)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddProfessionalAsync(Professional p)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region Get Infomation from database
+
+        public Task<Member> GetAAdminAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Admin>> GetAdminListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Member> GetAMemberAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Member> GetAProfessionalAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Member>> GetMemberListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Professional>> GetProfessionalListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
